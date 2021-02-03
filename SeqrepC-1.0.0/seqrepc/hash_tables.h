7e7155e1a17b42a8a381f5e8f76029dc4a69c61a
ednilsonlomazi <ednilson.lomazi.gt@gmail.com>
2021-02-03 15:11:44 -0300

    seqreppy

--- SeqrepC-1.0.0/seqrepc/hash_tables.h
+++
@@ -1,9 +0,0 @@
-#ifndef HASH_TABLES_H
-#define HASH_TABLES_H
-MpStruct* mp_hash_table[MAPPING_NUM] = {NULL, NULL, NULL, NULL, 
-										NULL, NULL, NULL, NULL, 
-										NULL, NULL, NULL, NULL, 
-										NULL, NULL, NULL, NULL}; 
-
-
-#endif // HASH_TABLES_H
--- SeqrepC-1.0.0/seqrepc/src/encoder/encoder.c
+++
@@ -1,4 +0,0 @@
-#include "../../encoder.h"
-
-#include "./mapping_one.c"
-#include "./mapping_many.c"
